4453cbaab14cf04db55dd1e996724c30e0d61eb6
espenoker <espen@studio-oker.com>
2021-04-15 14:56:45 +0200

    hover example

---
+++ slides.js
@@ -0,0 +1,34 @@
+let slideArea = document.querySelector("div.slides")
+let images = slideArea.querySelectorAll("img")
+
+let currentSlide = 0
+let zIndex = 1
+
+slideArea.addEventListener("click", function () {
+	currentSlide = currentSlide + 1
+  currentSlide = currentSlide % images.length
+  
+  zIndex = zIndex + 1
+  
+  images[currentSlide].style.zIndex = zIndex
+  
+  images.forEach(img => {
+    img.classList.remove("displayed")
+  })
+  
+  images[currentSlide].classList.add("displayed")
+})
+
+slideArea.addEventListener("mouseover", function () {
+  images.forEach(img => {
+    const x = 40 * Math.floor(Math.random() * 5 - 2)
+    const y = 40 * Math.floor(Math.random() * 5 - 2)
+    img.style.transform = `translate(${x}px, ${y}px)`
+  })
+})
+
+slideArea.addEventListener("mouseout", function () {
+  images.forEach(img => {
+    img.style.transform = ""
+  })
+})
